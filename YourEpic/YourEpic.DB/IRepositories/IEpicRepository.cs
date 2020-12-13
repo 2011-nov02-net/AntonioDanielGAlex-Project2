5c4db1d56968819077b26dbf724e72ce7fcb55d4
dannydjg16 <dannydjg16@gmail.com>
2020-12-13 17:40:50 -0500

    Made the interfaces for the Repositories

---
+++ YourEpic/YourEpic.DB/IRepositories/IEpicRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace YourEpic.DB.IRepositories
+{
+    public interface IEpicRepository
+    {
+        /// <summary>
+        /// Return the single epic you want to look at.
+        ///     make sure to return the chapters list in order of chapter
+        /// </summary>
+        /// <param name="epicID"></param>
+        /// <returns></returns>
+        public Domain.Epic GetEpicByID(int epicID);
+
+        /// <summary>
+        /// Get all of the epics that have been published
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<Domain.epic> GetAllEpics();
+
+        /// <summary>
+        /// When youre looking at a Publisher, use this to get all of his/her epics
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<Domain.epic> GetPublishersEpics(int publisherID);
+
+    }
+}
---
+++ YourEpic/YourEpic.DB/IRepositories/ISubscribtionsRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace YourEpic.DB.IRepositories
+{
+    public interface ISubscribtionsRepository
+    {
+        /// <summary>
+        /// Subscribe to a Publisher 
+        /// </summary>
+        /// <param name="publisherID"></param>
+        /// <param name="subscriberID"></param>
+        public void SubscribeToPublisher(int publisherID, int subscriberID);
+
+        /// <summary>
+        /// Unsubscribe From a Publisher
+        /// </summary>
+        /// <param name="publisherID"></param>
+        /// <param name="subscriberID"></param>
+        public void UnsubscribeFromPublisher(int publisherID, int subscriberID);
+    }
+}
