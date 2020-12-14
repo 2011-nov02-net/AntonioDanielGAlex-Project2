e17197669cc12508119f102326e6aef830e066de
dannydjg16 <dannydjg16@gmail.com>
2020-12-14 01:12:51 -0500

    Changed the Repo folder to domain, now add methods I dont have yet

--- YourEpic/YourEpic.Domain/Interfaces/IPublisherInterface.cs
+++ YourEpic/YourEpic.Domain/Interfaces/IPublisherInterface.cs
@@ -1,7 +1,26 @@
-﻿using System;
+﻿
+using System.Collections.Generic;
+using YourEpic.Domain.Models;
+
 namespace YourEpic.Domain.Interfaces
 {
     public interface IPublisherInterface
     {
+
+        public bool AddEpic(Epic epic);
+
+        public bool DeleteEpic(int epicID);
+
+        public bool AddChapter(Domain.Models.Chapter chapter);
+
+        public bool DeleteChapter(int chapterID);
+
+        public bool EditChapter(int chapterID);
+
+        public bool CategorizeEpic(int categoryID, int epicID);
+
+        public IEnumerable<Category> GetCategories();
+
+
     }
 }
---
+++ YourEpic/YourEpic.Domain/Interfaces/ISubscriptionRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace YourEpic.Domain.Interfaces
+{
+    public interface ISubscriptionRepository
+    {
+        public bool SubscribeToPublisher(User subscriber, User publisher);
+
+        public bool UnsubscribeFromPublisher(User subscriber, User publisher);
+    }
+}
