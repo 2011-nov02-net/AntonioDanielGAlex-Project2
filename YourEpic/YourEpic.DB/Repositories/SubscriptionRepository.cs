b6bb5a1c1ef0a7f5b5f61580823a13e0b69b2f29
Daniel Grant <dannydjg16@gmail.com>
2020-12-16 23:02:02 -0500

    Repo changes (#12)
    * New controller. Adding additional methods then changing interface stuff
    
    * Changed some messed up interface file names
    
    * Changed messed up scope
    
    * Finished first barebones UsersController
    
    * Most of this is adding/implementing the chapter Controller which I think looks pretty good. Also had to add a method in the Epic Repo
    
    * Added semicolon
    
    * Finished the Users and Chapters Controllers, onto the Epics Controller. I also deleted the WeatchForecastController
    
    * Made new controller and modified some ugliness in some others.
    
    * done with Subscription Repository, I took out one duplicate method that was also in the Reader Repository. Now I am going to finish Reader Repo and change the DDL of the DB
    
    * New Database structure. Comments has reply column, Epics has Completed column and concept column
    
    * Changed the repo I didn't realize was going to cause a problem when it called a method that didnt exist.
    
    * saved ddl file
    
    * using the context now

---
+++ YourEpic/YourEpic.DB/Repositories/SubscriptionRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using YourEpic.Domain.Interfaces;
+
+namespace YourEpic.DB.Repositories
+{
+    public class SubscriptionRepository: ISubscriptionRepository
+    {
+        private readonly YourEpicProjectTwoDatabaseContext _context;
+
+        public SubscriptionRepository(YourEpicProjectTwoDatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Domain.Models.Epic> GetMySubscriptions(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SubscribeToPublisher(int subscriberID, int publisherID)
+        {
+            var newSubscription = new Subscription { WriterId = publisherID, SubscriberId = subscriberID, HasNewContent = true };
+
+            _context.Add(newSubscription);
+
+            _context.SaveChanges();
+
+            return true;
+        }
+
+        public bool UnsubscribeFromPublisher(int subscriberID, int publisherID)
+        {
+            var deleteSubscription = _context.Subscriptions.Find(publisherID, subscriberID);
+            
+            _context.Subscriptions.Remove(deleteSubscription);
+
+            return true;
+        }
+    }
+}
