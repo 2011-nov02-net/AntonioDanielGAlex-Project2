578ceb1c926697e07827c87189951d1e9c6a7213
alexsoto24 <73846626+alexsoto24@users.noreply.github.com>
2020-12-17 08:39:30 -0800

    Implemented account, comment, and category repositories. (#14)
    * Implemented Account and Category Repos
    
    * Implemented Comment repo
    
    * switched rating from double to int
    
    * Changed Epic Model
    
    * fixed formatting issues
    
    * Added documentation to comment repository

--- YourEpic/YourEpic.DB/Repositories/AccountRepository.cs
+++ YourEpic/YourEpic.DB/Repositories/AccountRepository.cs
@@ -16,29 +16,171 @@ namespace YourEpic.DB.Repositories
         {
             _context = context ?? throw new ArgumentNullException(nameof(context));
         }
+
+        /// <summary>
+        /// Create a user account
+        /// </summary>
+        /// <param name="user">user to add to database</param>
+        /// <returns>True if user succesfully added to database or false if customer already exists</returns>
         public bool CreateAccount(Domain.Models.User user)
         {
-            return false;
+            var dbUser = _context.Users
+                .FirstOrDefault(u => u.Id == user.ID);
+
+            if(dbUser != null)
+            {
+                return false;
+            }
+
+            dbUser = new User
+            {
+                Id = user.ID,
+                Name = user.Name,
+                Role = user.UserRole.ID,
+                Email = user.Email
+            };
+
+            _context.Users.Add(dbUser);
+            _context.SaveChanges();
+
+            return true;
+            
         }
 
+        /// <summary>
+        /// Deletes user and related data from database.
+        /// </summary>
+        /// <param name="userID">User Id</param>
+        /// <returns>True if deltetion was succesful or false if not customer was found to delete</returns>
         public bool DeleteAccount(int userID)
         {
-            return false;
-        }
+            var dbUser = _context.Users
+                .FirstOrDefault(u => u.Id == userID);
 
+            if (dbUser == null)
+            {
+                return false;
+            }
+
+            _context.Users.Remove(dbUser);
+            _context.SaveChanges();
+
+            return true;
+        }
+        /// <summary>
+        /// Changes Name and Email of user in database.
+        /// </summary>
+        /// <param name="user">Domain.Models.User object</param>
+        /// <returns>True if edit was succesfull or false if not user was foundto edit</returns>
         public bool EditAccount(Domain.Models.User user)
         {
-            return false;
+            var dbUser = _context.Users
+                .FirstOrDefault(u => u.Id == user.ID);
+
+            if (dbUser == null)
+            {
+                return false;
+            }
+
+            dbUser.Name = user.Name;
+            dbUser.Email = user.Email;
+
+            _context.SaveChanges();
+
+            return true;
         }
 
+        /// <summary>
+        /// Gets users by name.
+        /// </summary>
+        /// <param name="name">Target user name</param>
+        /// <returns>users by search criteria, all users if no users match criteria or empty user list if no users exist</returns>
         public IEnumerable<Domain.Models.User> GetUsers(string name = null)
         {
-            return null;
+            List<User> dbUsers = new List<User>();
+
+            if(!string.IsNullOrWhiteSpace(name))
+            {
+                dbUsers = _context.Users
+                .Include(u => u.Epics)
+                .Include(u => u.RoleNavigation)
+                .Where(u => u.Name.Contains(name))
+                .ToList();
+            }
+            else
+            {
+                dbUsers = _context.Users
+                .Include(u => u.Epics)
+                .Include(u => u.RoleNavigation)
+                .ToList();
+            }
+
+            if (!dbUsers.Any())
+            {
+                return new List<Domain.Models.User>();
+            }
+            List<Domain.Models.User> users = dbUsers.Select(u => new Domain.Models.User
+            {
+                ID = u.Id,
+                Name = u.Name,
+                Email = u.Email,
+                UserRole = new Domain.Models.Role
+                {
+                    ID = u.RoleNavigation.Id,
+                    Name = u.RoleNavigation.Name
+                },
+                //User epics will only have Id, Title, and Date set here.
+                //If we want more details, get info through Epics repository.
+                Epics = u.Epics.Select(e => new Domain.Models.Epic
+                {
+                    ID = e.Id,
+                    Title = e.Name,
+                    Date = (DateTime)e.DateCreated
+                })
+            }).ToList();
+
+            return users;
         }
 
+        /// <summary>
+        /// Gets user by Id
+        /// </summary>
+        /// <param name="id">User Id</param>
+        /// <returns>Domain.Models.User object with epic info if exists in database, null otherwise</returns>
         public Domain.Models.User GetUserByID(int id)
         {
-            return null;
+            var dbUser = _context.Users
+                .Include(u => u.Epics)
+                .Include(u => u.RoleNavigation)
+                .FirstOrDefault(u => u.Id == id);
+
+            if(dbUser == null)
+            {
+                return null;
+            }
+
+            Domain.Models.User user = new Domain.Models.User
+            {
+                ID = dbUser.Id,
+                Name = dbUser.Name,
+                Email = dbUser.Email,
+                UserRole = new Domain.Models.Role
+                {
+                    ID = dbUser.RoleNavigation.Id,
+                    Name = dbUser.RoleNavigation.Name
+                },
+
+                //User epics will only have Id, Title, and Date set here.
+                //If we want more details, get info through Epics repository.
+                Epics = dbUser.Epics.Select(e => new Domain.Models.Epic
+                {
+                    ID = e.Id,
+                    Title = e.Name,
+                    Date = (DateTime)e.DateCreated
+                })
+            };
+
+            return user;
         }
     }
 }
--- YourEpic/YourEpic.DB/Repositories/CommentRepository.cs
+++ YourEpic/YourEpic.DB/Repositories/CommentRepository.cs
@@ -1,30 +1,107 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 using YourEpic.Domain.Interfaces;
+using YourEpic.Domain.Models;
 
 namespace YourEpic.DB.Repositories
 {
     public class CommentRepository : ICommentRepository
     {
+        private readonly YourEpicProjectTwoDatabaseContext _context;
+
+        public CommentRepository(YourEpicProjectTwoDatabaseContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        /// <summary>
+        /// Adds Comment to epic
+        /// </summary>
+        /// <param name="comment">Domain Model Object</param>
+        /// <returns>True if comment successfully added to epic in database</returns>
         public bool AddComment(Domain.Models.Comment comment)
         {
-            throw new NotImplementedException();
+            var dbComment = new Comment
+            {
+                Id = comment.ID,
+                CommenterId = comment.Commenter.ID,
+                EpicId = comment.CommentEpic.ID,
+                Comment1 = comment.CommentContent,
+                DateCreated = (DateTime)comment.Date
+            };
+
+            _context.Comments.Add(dbComment);
+            _context.SaveChanges();
+
+            return true;
         }
 
-        public bool DeleteComment(int commentID)
-        {
-            throw new NotImplementedException();
+        /// <summary>
+        /// Deletes Comment from database
+        /// </summary>
+        /// <param name="commentID">Comment id of comment to delete</param>
+        /// <returns>True if comment successfully deleted or false if comment non existent in database</returns>
+        public bool DeleteComment(int commentID) { 
+            var dbComment = _context.Comments
+                .FirstOrDefault(c => c.Id == commentID);
+
+            if (dbComment == null)
+            {
+                return false;
+            }
+
+            _context.Comments.Remove(dbComment);
+            _context.SaveChanges();
+
+            return true;
         }
 
+        /// <summary>
+        /// Gets all comments of an epic by id
+        /// </summary>
+        /// <param name="epicID">Target epic id</param>
+        /// <returns>All comments of an epic or empty list if epic has no comments yet</returns>
         public IEnumerable<Domain.Models.Comment> GetComments(int epicID)
         {
-            throw new NotImplementedException();
+            var dbComments = _context.Comments
+                .Include(c => c.Commenter)
+                .Include(c => c.Epic)
+                .Where(c => c.EpicId == epicID).ToList();
+
+            if (!dbComments.Any())
+            {
+                return new List<Domain.Models.Comment>();
+            }
+
+            List<Domain.Models.Comment> comments = dbComments.Select(c => new Domain.Models.Comment
+            {
+                ID = c.Id,
+                Commenter = new Domain.Models.User
+                {
+                    ID = c.Commenter.Id,
+                    Name = c.Commenter.Name,
+                    Email = c.Commenter.Email
+                },
+                CommentEpic = new Domain.Models.Epic
+                {
+                    ID = c.Epic.Id,
+                    Title = c.Epic.Name,
+                    Date = (DateTime)c.Epic.DateCreated
+                },
+                CommentContent = c.Comment1,
+                Date = (DateTime)c.DateCreated
+            }).ToList();
+            return comments;
         }
 
         public bool RespondToComment(int commentID, Domain.Models.Comment comment)
         {
-            throw new NotImplementedException();
+            //still need to implement this one
+            return false;
         }
+
     }
 }
