9ea0f677abc248b7925b824dede35d2fdcd8eab8
Daniel Grant <dannydjg16@gmail.com>
2020-12-17 16:26:01 -0500

    My finished Repos, I am a little unsure about get users subscription epics but I will change it if it ends up being wrong. (#15)

--- YourEpic/YourEpic.DB/Repositories/ReaderRepository.cs
+++ YourEpic/YourEpic.DB/Repositories/ReaderRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using YourEpic.Domain.Interfaces;
 
@@ -14,31 +15,72 @@ namespace YourEpic.DB.Repositories
             _context = context;
         }
 
-
-
         public IEnumerable<Domain.Models.Epic> GetEpicsSubscribedTo(int subscriberID)
         {
+            var epics = _context.Subscriptions.Where(s => s.SubscriberId == subscriberID)
+                .Join(_context.Epics, s => s.WriterId, e => e.WriterId, (s, e) => new Epic
+                {
+                    Id = e.Id,
+                    Name = e.Name,
+                    WriterId = e.WriterId,
+                    Concept = e.Concept,
+                    DateCreated = (DateTime)e.DateCreated
+                });
 
-            throw new NotImplementedException();
+            var domainEpics = epics.Select(e => new Domain.Models.Epic { ID = e.Id, Title = e.Name });
+
+
+            return domainEpics;
+
         }
 
         public bool MakeRating(Domain.Models.Rating rating)
         {
             _context.Ratings.Add(new Rating { RaterId = rating.Rater.ID, EpicId = rating.RatingEpic.ID, Rating1 = rating.RatingNumber });
             _context.SaveChanges();
-
-
-            throw new NotImplementedException();
+            return true;
         }
 
         public IEnumerable<Domain.Models.Epic> SearchForEpicByCategory(string category)
         {
-            throw new NotImplementedException();
+            var epics = _context.Categories.Where(c => c.Name.Contains(category))
+                .Join(_context.EpicCategories, c => c.Id, ec => ec.CategoryId, (c, ce) => new EpicCategory
+                {
+
+
+                    EpicId = ce.EpicId,
+                    CategoryId = c.Id
+
+
+
+                });
+
+            var ep = _context.Epics.Join(epics, e => e.Id, ep => ep.EpicId, (e, ep) => new Epic
+            {
+                Id = e.Id,
+                Name = e.Name,
+                WriterId = e.WriterId,
+                Concept = e.Concept,
+                DateCreated = (DateTime)e.DateCreated
+
+
+            });
+
+            var domainEpics = ep.Select(e => new Domain.Models.Epic { ID = e.Id, Title = e.Name });
+
+
+            return domainEpics;
         }
 
         public IEnumerable<Domain.Models.Epic> SearchForEpicByTitle(string Title)
         {
-            throw new NotImplementedException();
+            var epics = _context.Epics.Where(e => e.Name.Contains(Title));
+
+
+            var domainEpics = epics.Select(e => new Domain.Models.Epic { ID = e.Id, Title = e.Name});
+
+
+            return domainEpics;
         }
     }
 }
