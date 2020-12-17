86f483764017cf856242c60843802eda12c09b8a
Alex Soto <alex.soto0624@gmail.com>
2020-12-17 07:51:11 -0800

    fixed formatting issues

--- YourEpic/YourEpic.DB/Repositories/CommentRepository.cs
+++ YourEpic/YourEpic.DB/Repositories/CommentRepository.cs
@@ -33,9 +33,8 @@ namespace YourEpic.DB.Repositories
 
             return true;
         }
-
-        public bool DeleteComment(int commentID)
-        var dbComment = _context.Comments
+        public bool DeleteComment(int commentID) { 
+            var dbComment = _context.Comments
                 .FirstOrDefault(c => c.Id == commentID);
 
             if (dbComment == null)
@@ -47,46 +46,46 @@ namespace YourEpic.DB.Repositories
             _context.SaveChanges();
 
             return true;
-}
-public IEnumerable<Domain.Models.Comment> GetComments(int epicID)
-    {
-        var dbComments = _context.Comments
+        }
+
+        public IEnumerable<Domain.Models.Comment> GetComments(int epicID)
+        {
+            var dbComments = _context.Comments
                 .Include(c => c.Commenter)
                 .Include(c => c.Epic)
                 .Where(c => c.EpicId == epicID).ToList();
 
-        if (!dbComments.Any())
-        {
-            return new List<Domain.Models.Comment>();
+            if (!dbComments.Any())
+            {
+                return new List<Domain.Models.Comment>();
+            }
+
+            List<Domain.Models.Comment> comments = dbComments.Select(c => new Domain.Models.Comment
+            {
+                ID = c.Id,
+                Commenter = new Domain.Models.User
+                {
+                    ID = c.Commenter.Id,
+                    Name = c.Commenter.Name,
+                    Email = c.Commenter.Email
+                },
+                CommentEpic = new Domain.Models.Epic
+                {
+                    ID = c.Epic.Id,
+                    Title = c.Epic.Name,
+                    Date = (DateTime)c.Epic.DateCreated
+                },
+                CommentContent = c.Comment1,
+                Date = (DateTime)c.DateCreated
+            }).ToList();
+            return comments;
         }
 
-        List<Domain.Models.Comment> comments = dbComments.Select(c => new Domain.Models.Comment
-        {
-            ID = c.Id,
-            Commenter = new Domain.Models.User
-            {
-                ID = c.Commenter.Id,
-                Name = c.Commenter.Name,
-                Email = c.Commenter.Email
-            },
-            CommentEpic = new Domain.Models.Epic
-            {
-                ID = c.Epic.Id,
-                Title = c.Epic.Name,
-                Date = (DateTime)c.Epic.DateCreated
-            },
-            CommentContent = c.Comment1,
-            Date = (DateTime)c.DateCreated
-        }).ToList();
-
-        return comments;
-    }
-
         public bool RespondToComment(int commentID, Domain.Models.Comment comment)
-    {
-        //still need to implement this one
-        return false;
-    }
+        {
+            //still need to implement this one
+            return false;
+        }
 
     }
 }
