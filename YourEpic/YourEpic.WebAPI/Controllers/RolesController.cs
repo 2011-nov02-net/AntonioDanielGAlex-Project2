331604226a2c64929c567888344b34fe59e65a25
dannydjg16 <dannydjg16@gmail.com>
2020-12-18 00:50:34 -0500

    Added RolesController so that we can get roles when we need them

---
+++ YourEpic/YourEpic.WebAPI/Controllers/RolesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using YourEpic.Domain.Interfaces;
+using YourEpic.Domain.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace YourEpic.WebAPI.Controllers
+{ 
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RolesController : ControllerBase
+    {
+        private readonly IRolesRepository _rolesRepository;
+
+        public RolesController(IRolesRepository rolesRepository)
+        {
+            _rolesRepository = rolesRepository;
+        }
+    
+        // GET: api/roles
+        [HttpGet("{roles}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IEnumerable<Role>>> Get()
+        {
+            var roles = Task.FromResult(_rolesRepository.GetRoles());
+            if (roles is IEnumerable<Role>)
+            {
+                return Ok(roles);
+            }
+            return NotFound();
+        }
+    }
+}
