d544d666a04502bb647f0018bf055f104cc9a6dc
Alex Soto <alex.soto0624@gmail.com>
2020-12-19 02:28:47 -0800

    Implemented RatingModelMapper and RoleModelMapper

--- YourEpic/YourEpic.WebAPI/Mappers/RatingModelMapper.cs
+++ YourEpic/YourEpic.WebAPI/Mappers/RatingModelMapper.cs
@@ -1,10 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using YourEpic.WebAPI.Models;
+
 namespace YourEpic.WebAPI.Mappers
 {
-    public class RatingModelMapper
+    public static class RatingModelMapper
     {
-        public RatingModelMapper()
+        public static RatingModel Map(Domain.Models.Rating rating)
         {
+            return new RatingModel
+            {
+                ID = rating.ID,
+                RaterID = rating.Rater.ID,
+                EpicID = rating.RatingEpic.ID,
+                Rater = rating.Rater.Name,
+                RatingNumber = rating.RatingNumber
+            };
+        }
+
+        public static Domain.Models.Rating Map(RatingModel rating)
+        {
+            return new Domain.Models.Rating
+            {
+                ID = rating.ID,
+                Rater = new Domain.Models.User { ID = rating.RaterID, Name = rating.Rater },
+                RatingEpic = new Domain.Models.Epic { ID = rating.EpicID },
+                RatingNumber = rating.RatingNumber
+            };
         }
     }
 }
